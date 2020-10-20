ea1c46b355bfb38c4603c61005b6baffbc3cdab7
Amelia Elton <ameliareneeelton@gmail.com>
2020-10-20 18:29:32 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,7 +1,11 @@
 require_relative './part_1_solution.rb'
 
 def apply_coupons(cart, coupons)
- 
+ counter = 0 
+ while counter < coupons.length 
+  
+  counter += 1 
+ end
 end
 
 def apply_clearance(cart)
